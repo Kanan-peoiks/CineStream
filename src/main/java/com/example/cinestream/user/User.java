79c48a9d07c8d5125f76ff8430f666f331bd5983
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-19 16:14:19 +0400

    feat: add user classes

---
+++ src/main/java/com/example/cinestream/user/User.java
@@ -0,0 +1,29 @@
+package com.example.cinestream.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+}
